b3213789f5f4de0ba3ce86c779cdf9b391f0fcf9
sayed570 <73148150+sayed570@users.noreply.github.com>
2020-12-15 19:02:33 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -43,7 +43,7 @@ function draw() {
 }
 function keyPressed(){
 	if(keyCode === UP_ARROW){
-		Matter.body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:65,y:-65});
 	}
 }
 
